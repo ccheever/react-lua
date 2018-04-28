a253a0fe27269be8d400f13b0970e479fcf40dcd
Charlie Cheever <ccheever@gmail.com>
2018-04-28 16:05:42 -0700

    More tests for assign

--- src/__tests__/assign-test.lua
+++ src/__tests__/assign-test.lua
@@ -9,3 +9,6 @@ local object1 = {
 
 local object2 = assign({c = 4, d = 5}, object1)
 pi(object2.c, object2.d)
+
+local x = assign({}, {a = 1, b = 2, c = 3}, {"a", "b", "c"}, {b = 4, c = 5, d = 6})
+pi(x)
